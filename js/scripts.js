9521d80227be2d5024b4baf4f3846c0df81c580a
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-06-06 11:44:50 -0700

    Output all items in list
    Co-Authored-By: GarrettHays <103149986+GarrettHays@users.noreply.github.com>
    Co-Authored-By: AsheUrban <106103950+AsheUrban@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,34 @@ $(document).ready(function() {
     const animal = $("input#animal").val();
     const color = $("input#color").val();
     const number = $("input#number").val();
+    const hobby = $("input#hobby").val();
+    const sport = $("input#sport").val();
 
-    const favThings = [animal, color, number];
-      $("#output").text(favThings);
+    const favThings = [animal, color, number, hobby, sport];
+  
 
+    let listFavThings = "";
+    for(i=0; i<favThings.length; i++){
+        listFavThings +="<li>"+favThings[i]+"</li>";
+        }
+    $("#output").append(listFavThings);
+    
+    // /*
+    //   let favLen = allMyFavThings.length
+    //   for (let i = 0; i < favLen; i++) {
+    //     listFavThings += "<li>" + allMyFavThings[i] + "</li>";
+    //   }
+    //   */
+    // let listFavThings = "";
+    // allMyFavThings.forEach(favFunction);
+    // 
+
+    // function favFunction(value){
+    //   listFavThings += "<li>" + value + "</li>";
+    // }
+
+    //  // $("#output").text(listFavThings);
+
+    //   $('output').html(listFavThings); 	
   })
 });
